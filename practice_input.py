4bc8b13d443dca3c6e0cc91e9c4266f1944d3bf3
yongqingshannnn <yongqing.shann@gmail.com>
2024-05-28 23:00:03 +0800

    Made changes

--- practice_input.py
+++ practice_input.py
@@ -11,7 +11,9 @@ def get_favorite_vegetable():
     where X is replaced with the user's favorite vegetable.
     """
     # write your code here.
-
+get_favorite_vegetable = input("what's your favorite vegetable?")
+output_message = "Interesting! I also love" + get_favorite_vegetable +"!"
+print(output_message)
 
 def get_favorite_number():
     """
@@ -20,6 +22,9 @@ def get_favorite_number():
     where X is replaced with the user's favorite number.
     """
     # write your code here.
+get_favorite_number = input("what's your favorite number?")
+output_message = "Interesting! I also love" + get_favorite_number + "!"
+print(output_message)
 
 
 def get_name_and_zodiac_sign():
@@ -30,6 +35,11 @@ def get_name_and_zodiac_sign():
     where X and Y are replaced by the user's name and zodiac sign, respectively.
     """
     # write your code here.
+get_name = input("what's your name?")
+get_zodiac_sign = input("what's your zodiac sign?")
+output_message = "Interesting! My name is also" + get_name + ", and I'm also a" + get_zodiac_sign + "!"
+print(output_message)
+    
 
 
 def get_name_and_age():
@@ -40,3 +50,8 @@ def get_name_and_age():
     where X and Y are replaced by the user's name and age, respectively.
     """
     # write your code here.
+get_your_name = input("what is your name?")
+get_age = input("how old are you?")
+output_message = "Interesting! My name is also" + get_your_name + ", and I'm also" + get_age + "years old!"
+print(output_message)
+
--- practice_output.py
+++ practice_output.py
@@ -10,6 +10,7 @@ def print_with_line_break():
     Prints out the text, 'Hello world!' with a line break at the end
     """
     # write your code here
+print("Hello world!\n")
 
 
 def print_without_line_break():
@@ -17,6 +18,7 @@ def print_without_line_break():
     Prints out the text, 'Hello world!' without a line break at the end
     """
     # write your code here
+print("Hello world!")
 
 
 def print_with_separator_dash_and_with_line_break():
@@ -27,7 +29,12 @@ def print_with_separator_dash_and_with_line_break():
     """
     # write your code here
     # you must supply each word as a separate argument to the print() function
-
+print("Twas", end="-")
+print("brillig", end="-")
+print("and", end="-")
+print("the", end="-")
+print("slithy", end="-")
+print("toves\n") 
 
 def print_with_separator_dash_and_without_line_break():
     """
@@ -37,3 +44,9 @@ def print_with_separator_dash_and_without_line_break():
     """
     # write your code here
     # you must supply each word as a separate argument to the print() function
+print("Twas", end="-")
+print("brillig", end="-")
+print("and", end="-")
+print("the", end="-")
+print("slithy", end="-")
+print("toves") 
